ab6cef770fce4133edf0bac670ad9b0ef85bccfa
Joemel Bituin <bituinjoemel@gmail.com>
2020-11-09 02:45:41 +0800

    Add properties

--- lib/model/fuel_log.dart
+++ lib/model/fuel_log.dart
@@ -1,27 +1,41 @@
 class FuelLog {
-  FuelLog(this._distanceTravelled, this._fuelDate);
+  FuelLog(this._date, this._fuelAmount, this._fuelCost, this._vehicleId);
 
   int _id;
+  int _vehicleId;
   double _distanceTravelled;
-  String _fuelDate;
+  double _fuelAmount;
+  double _fuelCost;
+  String _date;
 
-  double get distanceTravelled => _distanceTravelled;
-  String get fuelDate => _fuelDate;
   int get id => _id;
+  int get vehicleId => _vehicleId;
+  double get distanceTravelled => _distanceTravelled;
+  double get fuelAmount => _fuelAmount;
+  double get fuelCost => _fuelCost;
+  String get date => _date;
+
+  set distanceTravelled(value) => _distanceTravelled = value;
 
   Map<String, dynamic> toMap() {
     var map = Map<String, dynamic>();
 
-    if(id != null) map['id'] = _id;
+    if (id != null) map['id'] = _id;
     map['distance_travelled'] = _distanceTravelled;
-    map['date'] = _fuelDate;
-    
+    map['date'] = _date;
+    map['fuel_amount'] = _fuelAmount;
+    map['fuel_cost'] = _fuelCost;
+    map['vehicle_id'] = _vehicleId;
+
     return map;
   }
 
   FuelLog.fromMapObject(Map<String, dynamic> map) {
     _id = map['id'] as int;
+    _vehicleId = map['vehicle_id'] as int;
     _distanceTravelled = map['distance_travelled'] as double;
-    _fuelDate = map['date'] as String;
+    _fuelAmount = map['fuel_amount'] as double;
+    _fuelCost = map['fuel_cost'] as double;
+    _date = map['date'] as String;
   }
 }
